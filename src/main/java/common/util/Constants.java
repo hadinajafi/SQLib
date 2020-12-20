170a9e5648bed1c50dfb84f3531869510d2e68f5
hadinajafi <hadinajafi.77@gmail.com>
2020-12-20 16:54:40 +0330

    added constants

---
+++ src/main/java/common/util/Constants.java
@@ -0,0 +1,15 @@
+package common.util;
+
+public class Constants {
+//    Errors ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    public static String IS_MISSING = "%s statement is missing!";
+    public static String NO_COLUMN_SELECTED = "No column selected!";
+    public static String REQ_PARAM_MISSING = "Required parameter is missing!";
+    public static String STATEMENT_BEFORE_ANOTHER = "%s statement can't come before %s statement!";
+    public static String WRONG_ORDER = "%s statement used in wrong order!";
+
+//    Query ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    public static String SELECT = "SELECT";
+    public static String FROM = "FROM";
+    public static String WHERE = "WHERE";
+}
