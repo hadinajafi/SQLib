82ea3732c72a8c816a6d9026ed7d5e77a3977101
hadinajafi <hadinajafi.77@gmail.com>
2021-01-26 20:57:44 +0330

    removed select interface

--- src/main/java/sqlib/query/Select.java
+++ src/main/java/sqlib/query/Select.java
@@ -1,12 +1,42 @@
 package sqlib.query;
 
+import common.exception.SQLibException;
 import sqlib.criteria.Result;
 
-public interface Select {
+import static common.util.Constants.SELECT;
+import static java.util.stream.IntStream.range;
 
-    Select select(String table, String... columns);
+public class Select extends Query{
 
-    Select select(Result clazz);
+    public Select select(String tableName, String... columns) {
+        if (columns == null || columns.length == 0)
+            throw new SQLibException("Columns parameter was empty, failed to select data.");
+        builder.append(SELECT).append(" ");
+        range(0, columns.length).forEach(i -> {
+            builder.append(columns[i]);
+            if (i != columns.length - 1)
+                builder.append(", ");
+            else
+                builder.append(" ");
+        });
+        querySoFar = builder.toString();
+        addFromClause(tableName);
+        return this;
+    }
 
-    String getQueryString();
+    public Select select(Result result) {
+        if (!result.hasFields())
+            throw new SQLibException("The provided class has no attributes, selection failed!");
+        builder.append(SELECT).append(" ");
+        range(0, result.size()).forEach(i -> {
+            builder.append(result.getColumnNames()[i]);
+            if (i != result.size() - 1)
+                builder.append(", ");
+            else
+                builder.append(" ");
+        });
+        querySoFar = builder.toString();
+        addFromClause(result.getClassName());
+        return this;
+    }
 }
